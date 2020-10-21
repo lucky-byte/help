c03bc6b8975d5408d63cfdc8ae5a1173d231ead7
xiaohu <xiaohu@lucky-byte.com>
2020-10-22 04:27:04 +0800

    修改项目基本配置，增加logo 和 favicon 文件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,82 +1,68 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: '帮助',
+  tagline: '功能说明，接口文档，示例代码',
+  organizationName: 'lucky-byte',
+  projectName: 'help',
   url: 'https://help.lucky-byte.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'lucky-byte', // Usually your GitHub org/user name.
-  projectName: 'help', // Usually your repo name.
+  favicon: 'img/favicon.png',
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'HELP',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Lucky Byte, Inc.',
+        src: 'img/lucky-byte.png',
       },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+      items: [{
+        to: 'docs/',
+        activeBasePath: 'docs',
+        label: '文档',
+        position: 'right',
+      }, {
+        to: 'blog',
+        label: '博客',
+        position: 'right',
+      }, {
+        href: 'https://github.com/lucky-byte/help',
+        label: 'GitHub',
+        position: 'right',
+      }],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      links: [{
+        title: '文档',
+        items: [{
+          label: '写作指南',
+          to: 'docs/',
+        }, {
+          label: 'Second Doc',
+          to: 'docs/doc2/',
+        }],
+      }, {
+        title: '社区',
+        items: [{
+          label: 'Stack Overflow',
+          href: 'https://stackoverflow.com/questions/tagged/luckybyte',
+        }, {
+          label: 'Discord',
+          href: 'https://discordapp.com/invite/luckybyte',
+        }, {
+          label: 'Twitter',
+          href: 'https://twitter.com/luckybyte',
+        }],
+      }, {
+        title: '更多',
+        items: [{
+          label: '博客',
+          to: 'blog',
+        }, {
+          label: 'GitHub',
+          href: 'https://github.com/lucky-byte/help',
+        }],
+      }],
+      copyright: `Copyright © ${new Date().getFullYear()} Lucky Byte, Inc.`,
     },
   },
   presets: [
@@ -85,15 +71,11 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: 'https://github.com/lucky-byte/help/edit/main/docs',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl: 'https://github.com/lucky-byte/help/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
